4ba2d46293ecc543b341f825dcccfc3e2d1d8a69
MuthuKumar.K <132651203+MuthuKumarkumar@users.noreply.github.com>
2025-03-27 18:40:12 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,22 @@
+
+const display=document.getElementById("display")
+
+function Display(input){
+    display.value+=input
+}
+
+function cleardisplay(){
+    display.value=""
+}
+
+function calculate(){
+    try{
+        display.value=eval(display.value)
+}
+
+    catch(error){
+        display.value=`ERROR`
+    }
+    
+    }
+    
